2b33d7af9e68e6bf15084f84752f3826762a4c99
Alfat393CSE <alfattasnimhasan@gmail.com>
2026-09-24 00:15:37 +0600

    initialize app.js for rendering apis in index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import dotenv from "dotenv";
-import express from "express";
-
+import app from "./app.js";
 dotenv.config({
   path: "./.env",
-});
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send(`Hello World`);
-});
-
-app.get("/instagram", (req, res) => {
-  res.send(`this is an instagram page`);
 });
 
 const port = process.env.PORT || 3000;
