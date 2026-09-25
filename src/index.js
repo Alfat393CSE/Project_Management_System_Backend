da797961b01b53b8e317b75ee311eb963dade45b
Alfat393CSE <alfattasnimhasan@gmail.com>
2026-09-25 18:54:22 +0600

    mongodb connect successfully

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./.env",
@@ -7,6 +8,15 @@ dotenv.config({
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`server running at http://localhost:${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server running at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+
+
